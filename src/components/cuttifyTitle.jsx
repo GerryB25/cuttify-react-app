60300a10b1faef2916a61662dfc0109d277ca069
gerryb <luisdelacruzg@tabasco.gob.mx>
2026-06-23 11:58:35 -0600

    fix: update main title to "Cuttify"

--- src/components/cuttifyTitle.jsx
+++ src/components/cuttifyTitle.jsx
@@ -3,7 +3,7 @@ import '../assets/css/cuttifyTitle.css'
 export function AcortadorTitle () {
   return (
         <header className="body-content">
-            <h1 className="main-title">Cuttify URL</h1>
+            <h1 className="main-title">Cuttify</h1>
             <div className='sub-texto'>
                 <p>Servicio acortador de links gratuito y sin publicidad (al menos por ahora)</p>
                 <p>Comparte links acortados de forma segura!</p>
